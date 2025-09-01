5b32455e2368aaf0574b350b08e8b90ef19353b2
ViniciusBezerra <vinicius.rbezerra@hotmail.com>
2025-09-01 10:04:23 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Tijolo", imei: "121212", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+//
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iPhone = new Iphone(numero: "987654321", modelo: "iPhone 17", imei: "222444", memoria: 256);
+iPhone.Ligar();
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Instagram");
